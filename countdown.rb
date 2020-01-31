d4cf670d376d21a2472f53f6bd1561ba67cf2017
Camisha  Farris <cfarris@anointedflooring.com>
2020-01-31 23:13:04 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,8 @@
 def countdown(seconds)
   while seconds < 0 
-  puts "#{seconds} SECOND(s)!"
-  seconds -= 1 
-end
-  
+    puts "#{seconds} SECOND(s)!"
+    seconds -= 1 
+ end
   "Happy New Year"
 end
 
